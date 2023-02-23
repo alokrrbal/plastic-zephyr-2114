2e123018db1a64cb1393b412520048e19525bf52
alokrrbal <alokrashmiranjan2@gmail.com>
2023-02-23 12:37:05 +0530

    day2

---
+++ cw-project/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Compononent/Navbar";
+
+
+function App() {
+  return (
+    <div>
+     <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ cw-project/src/Compononent/Navbar.js
@@ -0,0 +1,15 @@
+import react from "react"
+import './Navbar.css';
+const Navbar =  ()=>{
+    return(
+        <>
+            <nav className="nav-bar">
+                <div className="imgBox">
+                    
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
